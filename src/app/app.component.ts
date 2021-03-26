d1b1a7189c4d17c5fd26c917aa7a650cdd76b7a0
henry082 <herryoo82@gmail.com>
2021-03-26 18:39:34 +0800

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Todo } from './todo.model';
+
+const alltodos=[
+  new Todo('Hit the gym',false),
+  new Todo('Pay bills',true),
+  new Todo('Meet George',false),
+  new Todo('Buy eggs',false),
+  new Todo('Read a book',false),
+  new Todo('Organize office',false)
+]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TodoList';
+  title = 'todo';
+  alltodos=alltodos;
+  @ViewChild('new') new:ElementRef
+  onAdd(){
+    this.alltodos.push(new Todo(this.new.nativeElement.value,false))
+    this.new.nativeElement.value=null;
+  }
+  onChange(todo){
+    todo.isCheck = !todo.isCheck
+  }
+  onDele(i){
+    this.alltodos.splice(i,1)
+  }
 }
